6e7d3dd082703955d6ba829e2be7ea9436b48930
mrsekut <k.cloudspider@gmail.com>
2023-08-13 23:04:42 +0900

    scherulerの実装

--- packages/toy-scheduler/src/forks/ToyScheduler.js
+++ packages/toy-scheduler/src/forks/ToyScheduler.js
@@ -1,5 +1,26 @@
-'use strict';
+"use strict";
 
-function workLoop(deadline, commitRoot, performUnitOfWork, internals) {}
+/**
+ * Scheduler
+ * - どのタイミングでタスクを実行するかを制御する
+ */
 
-export function scheduleCallback(commitRoot, performUnitOfWork, internals) {}
+export function scheduleCallback(commitRoot, performUnitOfWork, internals) {
+	requestIdleCallback((deadline) => {
+		workLoop(deadline, commitRoot, performUnitOfWork, internals);
+	});
+}
+
+function workLoop(deadline, commitRoot, performUnitOfWork, internals) {
+	let shouldYield = false;
+	while (internals.nextUnitOfWork && !shouldYield) {
+		internals.nextUnitOfWork = performUnitOfWork(
+			internals.nextUnitOfWork,
+			internals
+		);
+		shouldYield = deadline.timeRemaining() < 1; // IdleDetalineが1msより小さい場合にのみタスクを実行するようにする
+	}
+	requestIdleCallback((deadline) => {
+		workLoop(deadline, commitRoot, performUnitOfWork, internals);
+	});
+}
